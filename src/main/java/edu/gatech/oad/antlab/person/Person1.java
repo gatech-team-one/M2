7d34835a4fbbf5ca88f6177fe1c2d5a6191d9447
Andie Rocca <rocca.andie@gmail.com>
2016-01-26 18:09:17 -0500

    changed Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -9,16 +9,16 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
+	/** Holds the persons real name */
+	private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
+	public Person1(String pname) {
+		name = pname;
+	}
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,8 +30,8 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		//Person 1 put your implementation here
+		return input.substring(2, input.length()) + input.substring(0, 2);
 	}
 	
 	/**
@@ -43,7 +43,7 @@ public class Person1 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 }
