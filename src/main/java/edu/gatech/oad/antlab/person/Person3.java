52f43a77f31741cc1a4be794ce5c3b699bd0fee1
Bombergames <johnwblum@gmail.com>
2016-01-29 13:58:15 -0500

    Person 3 reverses name
    finished, tested

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -2,15 +2,19 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 3
  *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
+ *  reversed string
+ *
+ *  @author  John
  *  @version 1.1
  */
+
+
+
 public class Person3 {
-   /** Holds the persons real name */  
+
+   /** Holds the persons real name */
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
@@ -19,19 +23,19 @@ public class Person3 {
 	public Person3(String pname){
 	  name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-	
+
 	/**
 	 * This method should take the string
 	 * input and return its reverse.
@@ -42,7 +46,13 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  	String output = "";
+
+		for (int i = input.length() - 1; i >= 0; i--)
+		{
+			output = output.concat(String.valueOf(input.charAt(i)));
+
+		}
+	  	return output;
 	}
 }
